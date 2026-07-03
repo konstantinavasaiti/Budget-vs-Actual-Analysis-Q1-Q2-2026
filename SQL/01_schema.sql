2afeecd3d330856b45d8c0423f85be662af125e5
konstantinavasaiti <konstantina.vasaiti@gmail.com>
2026-07-03 23:04:44 +0300

    Update 01_schema.sql

--- SQL/01_schema.sql
+++ SQL/01_schema.sql
@@ -10,18 +10,18 @@ CREATE TABLE expense_categories (
 
 CREATE TABLE budgets (
     budget_id INT PRIMARY KEY,
-    departments_id INT NOT NULL REFERENCES departments(department_id),
+    department_id INT NOT NULL REFERENCES departments(department_id),
     category_id INT NOT NULL REFERENCES expense_categories(category_id),
     month DATE NOT NULL,
     budget_amount NUMERIC(12, 2) NOT NULL,
-    UNIQUE(departments_id, category_id, month)
+    UNIQUE(department_id, category_id, month)
 );
 
 CREATE TABLE actuals (
     actual_id INT PRIMARY KEY,
-    departments_id INT NOT NULL REFERENCES departments(department_id),
+    department_id INT NOT NULL REFERENCES departments(department_id),
     category_id INT NOT NULL REFERENCES expense_categories(category_id),
     month DATE NOT NULL,
     actual_amount NUMERIC(12, 2) NOT NULL,
-    UNIQUE(departments_id, category_id, month)
+    UNIQUE(department_id, category_id, month)
 );
