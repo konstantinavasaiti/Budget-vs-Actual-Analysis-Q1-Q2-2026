d67a78be911a4e43aa8c8b3023a0589cfc086e81
konstantinavasaiti <konstantina.vasaiti@gmail.com>
2026-07-03 21:54:57 +0300

    Add files via upload

---
+++ SQL/02_seed_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO departments (department_id, department_name) VALUES (1, 'Sales');
+INSERT INTO departments (department_id, department_name) VALUES (2, 'Marketing');
+INSERT INTO departments (department_id, department_name) VALUES (3, 'Operations');
+INSERT INTO departments (department_id, department_name) VALUES (4, 'R&D');
+INSERT INTO departments (department_id, department_name) VALUES (5, 'HR');
+INSERT INTO departments (department_id, department_name) VALUES (6, 'IT');
+
+INSERT INTO expense_categories (category_id, category_name) VALUES (1, 'Salaries');
+INSERT INTO expense_categories (category_id, category_name) VALUES (2, 'Marketing Spend');
+INSERT INTO expense_categories (category_id, category_name) VALUES (3, 'Travel');
+INSERT INTO expense_categories (category_id, category_name) VALUES (4, 'Software');
+INSERT INTO expense_categories (category_id, category_name) VALUES (5, 'Equipment');
+INSERT INTO expense_categories (category_id, category_name) VALUES (6, 'Other');
