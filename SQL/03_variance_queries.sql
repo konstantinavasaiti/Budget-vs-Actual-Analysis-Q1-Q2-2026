eccfb5bd28e47bfbedf5f893aacc629d126adcf3
konstantinavasaiti <konstantina.vasaiti@gmail.com>
2026-07-04 13:27:06 +0300

    Add files via upload

---
+++ SQL/03_variance_queries.sql
@@ -0,0 +1,13 @@
+SELECT
+    d.department_name,
+    ROUND(SUM(b.budget_amount), 2) AS total_budget,
+    ROUND(SUM(a.actual_amount), 2) AS total_actual,
+    ROUND(SUM(a.actual_amount) - SUM(b.budget_amount), 2) AS variance
+FROM budgets b
+JOIN actuals a
+    ON b.department_id = a.department_id
+    AND b.category_id = a.category_id
+    AND b.month = a.month
+JOIN departments d ON d.department_id = b.department_id
+GROUP BY d.department_name
+ORDER BY variance DESC;
