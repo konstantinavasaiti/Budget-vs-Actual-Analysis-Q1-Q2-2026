ce79d13673d7e2a50d5a378f20f92eef62cb82f5
konstantinavasaiti <konstantina.vasaiti@gmail.com>
2026-07-08 12:30:36 +0300

    Add month to variance query and update grouping
    git add sql/03_variance_queries.sql
    git commit -m "Add monthly breakdown to variance query"
    git push

--- SQL/03_variance_queries.sql
+++ SQL/03_variance_queries.sql
@@ -1,5 +1,6 @@
 SELECT
     d.department_name,
+    b.month,
     ROUND(SUM(b.budget_amount), 2) AS total_budget,
     ROUND(SUM(a.actual_amount), 2) AS total_actual,
     ROUND(SUM(a.actual_amount) - SUM(b.budget_amount), 2) AS variance
@@ -9,5 +10,5 @@ JOIN actuals a
     AND b.category_id = a.category_id
     AND b.month = a.month
 JOIN departments d ON d.department_id = b.department_id
-GROUP BY d.department_name
-ORDER BY variance DESC;
+GROUP BY d.department_name, b.month
+ORDER BY d.department_name, b.month;
